559c101c2f51c293bf8afe6dfdd06ed3230834a0
Walter <walter@WalterGsMacBook.home>
2015-11-29 18:50:33 -0500

    added contact form and PHP routing

---
+++ index.php
@@ -0,0 +1,7 @@
+ <?php 
+// $_REQUEST - the data that is coming from the form, through AJAX 
+$params = $_REQUEST; 
+// Do something with the parameters, probably you would like to save them in your database 
+if (everything_went_ok) echo "Everything is ok. Your submission was saved"; 
+else (something_went_wrong) echo "There was a problem. Your submission could not be saved"; 
+?>
--- js/mycustom.js
+++ js/mycustom.js
@@ -7,4 +7,6 @@ $( document ).ready( function(){
 	  });
 
      $('#landingContent').fadeIn( 6000 );
+
+//   
 });
